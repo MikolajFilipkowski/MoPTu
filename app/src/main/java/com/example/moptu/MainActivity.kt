5b4afb59ea7a007e589f751740f843ea485dae53
Mikolaj Filipkowski <mikolajfilipkowski000@gmail.com>
2024-05-30 23:29:44 +0200

    Add Explore Fragment. Add animations.

--- app/src/main/java/com/example/moptu/MainActivity.kt
+++ app/src/main/java/com/example/moptu/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.moptu
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.FrameLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
@@ -34,5 +37,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.toolbar_menu, menu)
         return true;
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_home -> replaceFragment(HomeFragment())
+            R.id.action_explore -> replaceFragment(ExploreFragment(), true)
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun replaceFragment(fragment: Fragment, reverse:Boolean=false) {
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.setCustomAnimations(
+            if (reverse) R.anim.slide_in_left else R.anim.slide_in_right,
+            if (reverse) R.anim.slide_out_left else R.anim.slide_out_right,
+        )
+
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
     }
 }
